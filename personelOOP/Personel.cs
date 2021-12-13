15a2d543e0786369c421d1b87e98404b23f0652f
Selman GÖKTAŞ <selman@karatekin.edu.tr>
2021-12-13 21:31:41 +0300

    Add project files.

---
+++ personelOOP/Personel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personelOOP
+{
+    public class Personel
+    {
+        public string sicilNo { get; set; }
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public string izin { get; set; }
+    }
+}
