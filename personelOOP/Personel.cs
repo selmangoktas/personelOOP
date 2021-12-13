2dc9c45c1c71f3356a27dc660fbdc86b38654193
Selman GÖKTAŞ <selman@karatekin.edu.tr>
2021-12-13 22:25:25 +0300

    classlar eklendi

--- personelOOP/Personel.cs
+++ personelOOP/Personel.cs
@@ -8,9 +8,61 @@ namespace personelOOP
 {
     public class Personel
     {
-        public string sicilNo { get; set; }
-        public string ad { get; set; }
-        public string soyad { get; set; }
-        public string izin { get; set; }
+        private string _sicilNo;
+        private string _ad;
+        private string _soyad;
+        private int _ucret;
+        private int _izin;
+
+        public string sicilNo
+        {
+            get
+            {
+                return _sicilNo;
+            }
+            set
+            {
+                if (_ad.Length == 5)
+                    _sicilNo = value;
+
+            }
+        }
+        public string ad
+        {
+            get { return _ad; }
+            set { _ad = value; }
+        }
+
+        public string soyad
+        {
+            get { return _soyad; }
+            set { _soyad = value; }
+        }
+
+
+        public int ucret
+        {
+            get { return _ucret; }
+            set { _ucret = value; }
+        }
+
+        public int izin
+        {
+            get { return _izin; }
+            set { _izin = value; }
+        }
+        public string AdSoyadGetir()
+        {
+            return $"{ad} {soyad}";
+        }
+
+        public virtual int AylikHesapla()
+        {
+
+            int maasHesabi = (30 - izin) * 150;
+            return maasHesabi;
+        }
+
+
     }
 }
